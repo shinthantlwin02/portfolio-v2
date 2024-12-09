9f165176d1011ae476a9755c6187576f1cd62b1e
shinthantlwin02 <shinshinshinstl@gmail.com>
2024-12-09 15:17:56 +0630

    ui fix

--- components/pages/ProjectGallery.jsx
+++ components/pages/ProjectGallery.jsx
@@ -2,7 +2,7 @@ import { ProjectGalleryParallax } from "./ProjectGalleryParallax"
 
 const ProjectGallery = () => {
     return(
-        <div className="bg-[#050922] flex flex-col md:flex-row items-center gap-5 pl-10">
+        <div className="bg-[#050922] flex flex-col md:flex-row items-center gap-5 md:pl-10">
             <div className="text-white w-[300px] max-md:py-10">
                 <h1 className="text-3xl mb-2 text-[#FCFFFF] font-bold uppercase">Project Gallery</h1>
                 <p className="text-sm text-[#B4B4B4] max-md:text-center">This section highlights a selection of my most notable projects, demonstrating my skills and expertise in frontend development </p>
